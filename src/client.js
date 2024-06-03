199f94c8a97eaa440ae1a9f094056f94e2853905
ProgramLuke <85005142+ExeLuke666@users.noreply.github.com>
2024-06-03 13:52:10 -0400

    Update on client.js

--- src/client.js
+++ src/client.js
@@ -1,8 +1,5 @@
 const { FrameworkClient } = require('@eartharoid/dbf');
-const {
-	GatewayIntentBits,
-	Partials,
-} = require('discord.js');
+const { GatewayIntentBits, Partials } = require('discord.js');
 const { PrismaClient } = require('@prisma/client');
 const Keyv = require('keyv');
 const I18n = require('@eartharoid/i18n');
@@ -15,20 +12,23 @@ const ms = require('ms');
 
 module.exports = class Client extends FrameworkClient {
 	constructor(config, log) {
+		const intents = [
+			GatewayIntentBits.DirectMessages,
+			GatewayIntentBits.DirectMessageReactions,
+			GatewayIntentBits.DirectMessageTyping,
+			GatewayIntentBits.MessageContent,
+			GatewayIntentBits.Guilds,
+			GatewayIntentBits.GuildMembers,
+			GatewayIntentBits.GuildMessages,
+		];
+
+		if (process.env.PUBLIC_BOT !== 'true') {
+			intents.push(GatewayIntentBits.GuildPresences);
+		}
+
 		super(
 			{
-				intents: [
-					...[
-						GatewayIntentBits.DirectMessages,
-						GatewayIntentBits.DirectMessageReactions,
-						GatewayIntentBits.DirectMessageTyping,
-						GatewayIntentBits.MessageContent,
-						GatewayIntentBits.Guilds,
-						GatewayIntentBits.GuildMembers,
-						GatewayIntentBits.GuildMessages,
-					],
-					...(process.env.PUBLIC_BOT !== 'true' ? [GatewayIntentBits.GuildPresences] : []),
-				],
+				intents,
 				partials: [
 					Partials.Channel,
 					Partials.Message,
@@ -39,58 +39,62 @@ module.exports = class Client extends FrameworkClient {
 			{ baseDir: __dirname },
 		);
 
+		this.config = config;
+		this.log = log;
+		this.supers = (process.env.SUPER ?? '').split(',');
+
+		this.locales = this.loadLocales();
+		this.keyv = new Keyv();
+		this.i18n = new I18n('en-GB', this.locales);
+		this.tickets = new TicketManager(this);
+
+		this.commands.interceptor = this.handleInteraction.bind(this);
+	}
+
+	loadLocales() {
 		const locales = {};
 		fs.readdirSync(join(__dirname, 'i18n'))
 			.filter(file => file.endsWith('.yml'))
 			.forEach(file => {
-				const data = fs.readFileSync(join(__dirname, 'i18n/' + file), { encoding: 'utf8' });
-				const name = file.slice(0, file.length - 4);
+				const data = fs.readFileSync(join(__dirname, 'i18n', file), 'utf8');
+				const name = file.slice(0, -4);
 				locales[name] = YAML.parse(data);
 			});
+		return locales;
+	}
 
-		this.keyv = new Keyv();
-		/** @type {I18n} */
-		this.i18n = new I18n('en-GB', locales);
-		/** @type {TicketManager} */
-		this.tickets = new TicketManager(this);
-		this.config = config;
-		this.log = log;
-		this.supers = (process.env.SUPER ?? '').split(',');
-		/** @param {import('discord.js/typings').Interaction} interaction */
-		this.commands.interceptor = async interaction => {
-			if (!interaction.inGuild()) return;
-			const id = interaction.guildId;
-			const cacheKey = `cache/known/guild:${id}`;
-			if (await this.keyv.has(cacheKey)) return;
-			await this.prisma.guild.upsert({
-				create: {
-					id,
-					locale: this.i18n.locales.find(locale => locale === interaction.guild.preferredLocale), // undefined if not supported
-				},
-				update: {},
-				where: { id },
-			});
-			await this.keyv.set(cacheKey, true);
-		};
+	async handleInteraction(interaction) {
+		if (!interaction.inGuild()) return;
+		const id = interaction.guildId;
+		const cacheKey = `cache/known/guild:${id}`;
+
+		if (await this.keyv.has(cacheKey)) return;
+
+		await this.prisma.guild.upsert({
+			create: {
+				id,
+				locale: this.i18n.locales.find(locale => locale === interaction.guild.preferredLocale),
+			},
+			update: {},
+			where: { id },
+		});
+
+		await this.keyv.set(cacheKey, true);
 	}
 
 	async login(token) {
 		const levels = ['error', 'info', 'warn'];
 		if (this.config.logs.level === 'debug') levels.push('query');
 
-		const prisma_options = {
-			log: levels.map(level => ({
-				emit: 'event',
-				level,
-			})),
+		const prismaOptions = {
+			log: levels.map(level => ({ emit: 'event', level })),
 		};
 
 		if (process.env.DB_PROVIDER === 'sqlite' && !process.env.DB_CONNECTION_URL) {
-			prisma_options.datasources = { db: { url:'file:' + join(process.cwd(), './user/database.db') } };
+			prismaOptions.datasources = { db: { url: `file:${join(process.cwd(), './user/database.db')}` } };
 		}
 
-		/** @type {PrismaClient} */
-		this.prisma = new PrismaClient(prisma_options);
+		this.prisma = new PrismaClient(prismaOptions);
 
 		this.prisma.$on('error', e => this.log.error.prisma(`${e.target} ${e.message}`));
 		this.prisma.$on('info', e => this.log.info.prisma(`${e.target} ${e.message}`));
@@ -98,14 +102,12 @@ module.exports = class Client extends FrameworkClient {
 		this.prisma.$on('query', e => this.log.debug.prisma(e));
 
 		if (process.env.DB_PROVIDER === 'sqlite') {
-			// rewrite queries that use unsupported features
 			this.prisma.$use(sqliteMiddleware);
-			// make sqlite faster (missing parentheses are not a mistake, `$queryRaw` is a tagged template literal)
-			this.log.debug(await this.prisma.$queryRaw`PRAGMA journal_mode=WAL;`); // https://www.sqlite.org/wal.html
-			this.log.debug(await this.prisma.$queryRaw`PRAGMA synchronous=normal;`); // https://www.sqlite.org/pragma.html#pragma_synchronous
+			this.log.debug(await this.prisma.$queryRaw`PRAGMA journal_mode=WAL;`);
+			this.log.debug(await this.prisma.$queryRaw`PRAGMA synchronous=normal;`);
 
 			setInterval(async () => {
-				this.log.debug(await this.prisma.$queryRaw`PRAGMA optimize;`); // https://www.sqlite.org/pragma.html#pragma_optimize
+				this.log.debug(await this.prisma.$queryRaw`PRAGMA optimize;`);
 			}, ms('6h'));
 		}
 
